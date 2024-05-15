2b98efc535a02cf1b02e1fccec88a71c1ac604e2
Rhea Chainani <112756676+rheachainani@users.noreply.github.com>
2024-05-15 12:21:12 +0530

    Update MyAccountException.java

--- myaccount/MyAccountException.java
+++ myaccount/MyAccountException.java
@@ -1,20 +1,25 @@
 package myaccount;
+import utility.Utility;
 
 public class MyAccountException extends Exception {
     
     public MyAccountException() {
         super();
+        Utility.showAlert("Error","An error has occurred");
     }
     
     public MyAccountException(String message) {
         super(message);
+        Utility.showAlert("Error", message);
     }
     
     public MyAccountException(String message, Throwable cause) {
         super(message, cause);
+        Utility.showAlert("Error", message);
     }
     
     public MyAccountException(Throwable cause) {
         super(cause);
+        Utility.showAlert("Error","An error has occurred");
     }
 }
